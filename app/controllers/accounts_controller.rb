c4470997522207fb381990ea96e77725e1a31bfa
Matthew Todd <matthew.todd@gmail.com>
2009-08-27 14:48:13 +0300

    Ability to delete Accounts.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,6 @@
 class AccountsController < ApplicationController
-  before_filter :load_host, :only => [:new, :create]
+  before_filter :load_host,    :only => [:new, :create]
+  before_filter :load_account, :only => [:show, :edit, :update, :destroy]
 
   def create
     @account = @host.accounts.build(params[:account])
@@ -10,16 +11,7 @@ class AccountsController < ApplicationController
     end
   end
 
-  def show
-    @account = Account.find(params[:id])
-  end
-
-  def edit
-    @account = Account.find(params[:id])
-  end
-
   def update
-    @account = Account.find(params[:id])
     if @account.update_attributes(params[:account])
       redirect_to @account
     else
@@ -27,9 +19,18 @@ class AccountsController < ApplicationController
     end
   end
 
+  def destroy
+    @account.destroy
+    redirect_to @account.host
+  end
+
   private
 
   def load_host
     @host = Host.find(params[:host_id])
+  end
+
+  def load_account
+    @account = Account.find(params[:id])
   end
 end
