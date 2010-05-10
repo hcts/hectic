df686ac5403565e580e6d1b377b1b6d6245dcc8f
Matthew Todd <matthew.todd@gmail.com>
2010-05-10 11:53:10 +0300

    Only install production gems on deploy.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,5 +1,5 @@
 Chef::Log.info "bundling gems: #{latest_release}"
-chef_run("cd #{latest_release} && sudo -u #{user} gem bundle")
+chef_run("cd #{latest_release} && sudo -u #{user} gem bundle --only production")
 
 Chef::Log.info "updating crontab: #{latest_release}"
 chef_run("cd #{latest_release} && sudo -u #{user} ./bin/whenever --update-crontab hectic")
